4264e40ec31efb96bc8027b913496f01d3b57edf
cristianofull <cristianofull@gmail.com>
2023-03-06 18:27:19 -0300

    Trabalhando com métodos

---
+++ dio-trilha-java-basico/src/SmartTv.java
@@ -0,0 +1,38 @@
+public class SmartTv {
+	boolean ligada = false;
+	int canal = 1;
+	int volume = 25;
+	
+	
+	public void ligar() {
+		ligada = true;
+	}
+	
+	public void desligar() {
+		ligada = false;
+	}
+	
+	public void aumentarVolume() {
+		volume++;
+		System.out.println("Almentar o volume para: " + volume);
+	}
+	
+	public void diminuirVolume() {
+		volume--;
+		System.out.println("Diminuir o volume para: " + volume);
+	}
+	
+	public void mudarCanal(int novoCanal) {
+		canal = novoCanal;
+	}
+	
+	public void aumentarCanal() {
+		canal++;
+		System.out.println("Almentar o canal para: " + canal);
+	}
+	
+	public void diminuirCanal() {
+		canal--;
+		System.out.println("Diminuir o canal para: " + canal);
+	}
+}
